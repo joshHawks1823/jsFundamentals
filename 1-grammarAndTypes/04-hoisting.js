f9542b72cabef646dd30fb7718e3d1085e6fb3fa
joshhawks1823 <jheat23@gmail.com>
2019-10-23 14:28:10 -0400

    intial commit

---
+++ 1-grammarAndTypes/04-hoisting.js
@@ -0,0 +1,28 @@
+// Example 1
+console.log(scisscors);
+var scisscors = blue;
+
+// example 1:
+var scisscors;
+console.log(scisscors);
+scisscors = blue;
+
+// example 2:
+
+function hoistTest() {
+  console.log(testVar);
+  var testVar = 10;
+  console.log(testVar);
+}
+
+hoistTest();
+
+//example 2 breakdown:
+function hoistTest() {
+  var testVar;
+  console.log(testVar);
+  testVar = 10;
+  console.log(testVar);
+}
+
+hoistTest();
---
+++ 1-grammarAndTypes/06-literals.js
@@ -0,0 +1,42 @@
+// Literals
+
+let weekDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+console.log(weekDays.toString());
+console.log(weekDays[3]);
+
+let soup = {
+  a: "chicken noodle",
+  b: "tomato",
+  c: "beef and barley"
+};
+
+console.log(soup.c);
+
+// array and object literals can hold primitive and complex types
+
+let complexArrLiteral = [
+  1,
+  "Academy",
+  [1, 2, 3],
+  { key: "test" },
+  function() {
+    return "yes";
+  }
+];
+
+console.log(complexArrLiteral[4]);
+console.log(complexArrLiteral[2]);
+
+complexObjLiteral = {
+  num: 1,
+  boolean: true,
+  string: "Academy",
+  arr: [1, 2, 3],
+  obj: { key: "test" },
+  func: function() {
+    return "yes";
+  }
+};
+
+console.log(complexObjLiteral.func);
+console.log(complexObjLiteral.arr);
